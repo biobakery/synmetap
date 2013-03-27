8bed1639f348c4d5211e9c289c6544878207dfff
Boyu Ren <bor158@mail.harvard.edu>
2013-03-27 19:47:31 -0400

    Test modified check_contig.py. Fix bugs concerning force symlink.

--- src/check_contig.py
+++ src/check_contig.py
@@ -5,76 +5,52 @@ import sys
 import os
 import csv
 import argparse
-import os, errno
 
-def del_short_contig( fileIn, fileOut ):
+def del_short_contig( strfileIn, strfileOut ):
+	
+	try:
+		fileIn = open( strfileIn, "r" )
+	except IOError:
+		print "Cannot open IMG genome file."
+	try:
+		fileOut = open( strfileOut, "w" )
+	except IOError:
+		print "Cannot access output checked file."
+		
 	dline = 0
 	dhead = 0
 	dshort = 0
-	astrline_cache = []
+	dseqs = 0
+	aastrline_cache = []
 		
 	csv_fasta_in = csv.reader( fileIn, csv.excel_tab )
-	csv_fasta_out = csv.reader( fileOut, csv.excel_tab )
+	csv_fasta_out = csv.writer( fileOut, csv.excel_tab )
 
 	for astrLine in csv_fasta_in:
 		dline += 1
-		if re.search( r'^>', astrLine[0] ):
+		if ( astrLine and re.search( r'^>', astrLine[0] ) ):
+			dseqs += 1
 			if dhead == 0:
 				dhead = dline
 			else:
 				if dline - dhead - 2 >= 15:
-					csv_fasta_out.writerows( astrLine_cache )
+					csv_fasta_out.writerows( aastrLine_cache )
 				else:
 					dshort += 1
 				dhead = dline
-			astrline_cache = astrLine
+			aastrLine_cache = [astrLine]
+		else:
+			aastrLine_cache += [astrLine]
 	if dline - dhead - 2 >= 15:
-		csv_fasta_out.writerows( astrLine_cache )
+		csv_fasta_out.writerows( aastrLine_cache )
 	else:
 		dshort += 1
+	if not dshort:
+		os.remove( strfileOut )
+	return [dshort, dseqs]
 
-	return dshort
-"""
-def contig_pick( hashIndex ):
-
-	#start line number for short contig. 1 based
-	aRetS = []
-	#end line number for short contig. 1 based
-	aRetE = []
-
-	for key in hashIndex.keys():
-		aRetS += [key]
-	aRetS = sorted(aRetS)
-	aRetE = [hashIndex[key] for key in aRetS]
-	return [aRetS, aRetE]
-
-def contig_copy( genome_in, genome_out, aaDel ):
-	
-	#aaDel is [aRetS, aRetE]
-	aStart = aaDel[0]
-	aEnd = aaDel[1]
-
-	line_num = 0
-	try:
-		genome_file_in = open( genome_in, "r" )
-	except IOError:
-		print "Cannot open input genome fasta file."
-	try:
-		genome_file_out = open( genome_out, "w" )
-	except IOError:
-		print "Cannot access output genome fasta file."
-
-	for line in genome_file_in:
-		line_num += 1
-		line_pointer = bisect.bisect( aStart, line_num ) - 1 if line_num<= aStart[-1] else len( aStart ) - 1
-		if line_pointer < 0 or line_num > aEnd[line_pointer]:
-			genome_file_out.write( line )
-	
-	genome_file_in.close()
-	genome_file_out.close()
-"""
-			
 def _main():
+
 	#needed input: input files from the converted step(call it input_ref). Path to the raw genomes(rawGenome_path), path to the checked genome(checkedGenome_path)
 	#input_ref should be the full path for the reference file
 	#Use argparser.
@@ -101,20 +77,20 @@ def _main():
 		print "Cannot open input converted abundance file!"
 
 	csv_ref_in = csv.reader( refFile, csv.excel_tab )	
+	
+	print "IMG Taxon ID\t# Total Contigs\t# Short Contigs"
 
 	for astrLine in csv_ref_in:
 		
 		strinput_Genome = rawGenome_path + astrLine[0]
 		stroutput_Genome = checkedGenome_path + astrLine[0]
+		if os.path.exists( stroutput_Genome ):
+			os.remove( stroutput_Genome )
 
-		dShort = del_short_contig( open( strinput_Genome, "r" ), open( stroutput_Genome, "w"  ) )
+		dShort, dSeqs = del_short_contig( strinput_Genome, stroutput_Genome )
 		if dShort == 0:
-			try:
-				os.symlink( strinput_Genome, stroutput_Genome )
-			except OSError, e:
-				if e.errno == errno.EEXIST:
-					os.remove( stroutput_Genome )
-					os.symlink( strinput_Genome, stroutput_Genome )
+			os.symlink( strinput_Genome, stroutput_Genome )
+		print astrLine[0] + "\t" + str(dSeqs) + "\t" + str(dShort)
 	
 if __name__ == "__main__":
 	_main()
